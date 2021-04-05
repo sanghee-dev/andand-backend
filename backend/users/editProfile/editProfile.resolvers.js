c7febf4164e3c90a3084fb1d0c826bf4cd199798
leeedev <leeesangheee@gmail.com>
2021-04-05 19:14:45 +0900

    editProfile

--- backend/users/editProfile/editProfile.resolvers.js
+++ backend/users/editProfile/editProfile.resolvers.js
@@ -1,5 +1,33 @@
+import client from "../../client";
+import bcrypt from "bcrypt";
+
 export default {
   Mutation: {
-    editProfile: () => console.log("editProfile"),
+    editProfile: async (
+      _,
+      { firstName, lastName, username, email, password: newPassword }
+    ) => {
+      let uglyPassword = null;
+      if (newPassword) {
+        uglyPassword = await bcrypt.hash(newPassword, 10);
+      }
+      const updatedUser = client.user.update({
+        where: {
+          id: 1,
+        },
+        data: {
+          firstName,
+          lastName,
+          username,
+          email,
+          ...(uglyPassword && { password: uglyPassword }),
+        },
+      });
+      if (updatedUser) {
+        return { ok: true };
+      } else {
+        return { ok: false, error: "Could not update profile." };
+      }
+    },
   },
 };
