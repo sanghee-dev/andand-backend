e9a1c758b08a600d3b80441de995963fde11aec5
leeedev <leeesangheee@gmail.com>
2021-04-08 18:23:15 +0900

    Follow User

---
+++ src/users/followUser/followUser.resolvers.ts
@@ -0,0 +1,47 @@
+import { protectResolver } from "../users.utils";
+import { Resolvers } from "../../types";
+import client from "../../client";
+
+const resolverFn = async (_, { username }, { loggedInUser }) => {
+  try {
+    const ok = await client.user.findUnique({
+      where: {
+        username,
+      },
+    });
+    if (!ok) {
+      return {
+        ok: false,
+        error: "That user does not exist.",
+      };
+    }
+    await client.user.update({
+      where: {
+        id: loggedInUser.id,
+      },
+      data: {
+        following: {
+          connect: {
+            username,
+          },
+        },
+      },
+    });
+    return {
+      ok: true,
+    };
+  } catch (e) {
+    return {
+      ok: false,
+      eror: "Cannot follow user.",
+    };
+  }
+};
+
+const resolvers: Resolvers = {
+  Mutation: {
+    followUser: protectResolver(resolverFn),
+  },
+};
+
+export default resolvers;
