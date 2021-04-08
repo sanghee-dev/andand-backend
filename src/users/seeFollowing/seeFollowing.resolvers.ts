38f030a2384dbb761a480d5c9dd87615eab1fea7
leeedev <leeesangheee@gmail.com>
2021-04-09 00:57:12 +0900

    Following Pagination(cursor-based)

---
+++ src/users/seeFollowing/seeFollowing.resolvers.ts
@@ -0,0 +1,45 @@
+import { Resolvers } from "../../types";
+import client from "../../client";
+
+const resolvers: Resolvers = {
+  Query: {
+    seeFollowing: async (_, { username, lastId }) => {
+      try {
+        const ok = await client.user.findUnique({
+          where: { username },
+          select: { id: true },
+        });
+        if (!ok) {
+          return {
+            ok: false,
+            error: "User not found.",
+          };
+        }
+        const following = await client.user
+          .findUnique({ where: { username } })
+          .following({
+            skip: lastId ? 1 : 0,
+            take: 5,
+            ...(lastId && { cursor: { id: lastId } }),
+          });
+        const totalFollowing = await client.user.count({
+          where: {
+            followers: { some: { username } },
+          },
+        });
+        return {
+          ok: true,
+          following,
+          totalPages: Math.ceil(totalFollowing / 5),
+        };
+      } catch (e) {
+        return {
+          ok: false,
+          eror: "Cannot find following.",
+        };
+      }
+    },
+  },
+};
+
+export default resolvers;
