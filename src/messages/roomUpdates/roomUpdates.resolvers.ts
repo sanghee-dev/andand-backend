1fa36a5da8707c9e49bdcedd5702bebfc59c6d1e
leeedev <leeesangheee@gmail.com>
2021-04-17 01:29:29 +0900

    Authenticate Subscription

--- src/messages/roomUpdates/roomUpdates.resolvers.ts
+++ src/messages/roomUpdates/roomUpdates.resolvers.ts
@@ -7,9 +7,15 @@ const resolvers = {
   Subscription: {
     roomUpdates: {
       subscribe: async (root, args, context, info) => {
-        const room = await client.room.findUnique({
+        console.log(context);
+        const room = await client.room.findFirst({
           where: {
             id: args.id,
+            users: {
+              some: {
+                id: context.loggedInUser.id,
+              },
+            },
           },
           select: {
             id: true,
@@ -20,8 +26,26 @@ const resolvers = {
         }
         return withFilter(
           () => pubsub.asyncIterator(NEW_MESSAGE),
-          ({ roomUpdates }, { id }) => {
-            return roomUpdates.roomId === id;
+          async ({ roomUpdates }, { id }, { loggedInUser }) => {
+            if (roomUpdates.roomId === id) {
+              const room = await client.room.findFirst({
+                where: {
+                  id,
+                  users: {
+                    some: {
+                      id: loggedInUser.id,
+                    },
+                  },
+                },
+                select: {
+                  id: true,
+                },
+              });
+              if (!room) {
+                return false;
+              }
+              return true;
+            }
           }
         )(root, args, context, info);
       },
