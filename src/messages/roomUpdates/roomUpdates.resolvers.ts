0594011e66436f30ea6c015e1d491bf18d380417
leeedev <leeesangheee@gmail.com>
2021-04-17 00:28:31 +0900

    Filter Subscriptions(Check if the room exists)

--- src/messages/roomUpdates/roomUpdates.resolvers.ts
+++ src/messages/roomUpdates/roomUpdates.resolvers.ts
@@ -1,16 +1,30 @@
 import pubsub from "../../pubsub";
 import { withFilter } from "apollo-server-express";
 import { NEW_MESSAGE } from "../../constants";
+import client from "../../client";
 
 const resolvers = {
   Subscription: {
     roomUpdates: {
-      subscribe: withFilter(
-        () => pubsub.asyncIterator(NEW_MESSAGE),
-        ({ roomUpdates }, { id }) => {
-          return roomUpdates.roomId === id;
+      subscribe: async (root, args, context, info) => {
+        const room = await client.room.findUnique({
+          where: {
+            id: args.id,
+          },
+          select: {
+            id: true,
+          },
+        });
+        if (!room) {
+          throw new Error("You shall not see this.");
         }
-      ),
+        return withFilter(
+          () => pubsub.asyncIterator(NEW_MESSAGE),
+          ({ roomUpdates }, { id }) => {
+            return roomUpdates.roomId === id;
+          }
+        )(root, args, context, info);
+      },
     },
   },
 };
